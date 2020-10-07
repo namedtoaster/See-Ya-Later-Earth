8331f3385082a98c529312adbf58912ce5fbead0
Dave Campbell <djcampbell1914@gmail.com>
2020-10-07 19:43:48 -0400

    Complete part of level 2

--- Levels/Level1.Mars/Level1.Mars.gd
+++ Levels/Level1.Mars/Level1.Mars.gd
@@ -1,21 +1,11 @@
-extends Node2D
+extends BaseLevel
 
-var intro_dialog = null
-#["Welcome to the game",
-#"Do this, do that",
-#"Don't do this, don't do that",
-#"This is how you do this",
-#"This is how you do that",
-#"Good luck"]
+func _ready():	
+	._ready()
 
-export(String) var NEXT_SCENE
-
-func _ready():
-	$GUI.change_level(1)
+func _on_dialog_exit_before_fade():
+	var dialog_node = get_node("GUI/Dialog")
+	var current_dialog_name = dialog_node.calling_node.name
 	
-	# Every time this node loads in a new scene, dialog needs to process input in order to check if player is on floor
-	$GUI/Dialog.set_process(true)
-
-func _on_LevelEnd_body_entered(body):
-	if (body.name == "Player"):
-		assert(get_tree().change_scene(NEXT_SCENE) == OK)
+	if current_dialog_name == "Land":
+		dialog_node.calling_node.queue_free()
