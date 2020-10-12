9b640341c0d79036e73ac7cd21a82294685582ff
namedtoaster <djcampbell1914@gmail.com>
2020-10-11 20:36:15 -0400

    Update tether

--- Other/Objects/Rope/Area2D.gd
+++ Other/Objects/Rope/Area2D.gd
@@ -2,7 +2,8 @@ extends Area2D
 
 func _on_Area2D_body_entered(body):
 	if body.name == "Player":
-		var link = get_owner().get_node("Anchor/RopeLink/RopePoint/RopeLink2/RopePoint/RopeLink3")
+		var link = get_parent().get_node("RopeLink3")
 		link.node_a = get_parent().get_path()
 		link.node_b = body.get_path()
 		body.attached = true
+		get_owner().get_parent().get_owner().get_node("Area2D/CollisionPolygon2D").set_deferred("disabled", false)
